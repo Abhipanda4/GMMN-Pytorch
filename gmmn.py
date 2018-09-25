47fb515de423107f47bfd3947ec861b68bd962d1
abhipanda4 <abhipanda03@gmail.com>
2018-09-25 06:44:40 +0530

    activation changed to sigmoid

--- gmmn.py
+++ gmmn.py
@@ -5,12 +5,14 @@ import torch.nn.functional as F
 class GMMN(nn.Module):
     def __init__(self, n_start, n_inp):
         super(GMMN, self).__init__()
-        self.fc1 = nn.Linear(n_start, 256)
-        self.fc2 = nn.Linear(256, 512)
-        self.fc3 = nn.Linear(512, n_inp)
+        self.fc1 = nn.Linear(n_start, 64)
+        self.fc2 = nn.Linear(64, 256)
+        self.fc3 = nn.Linear(256, 256)
+        self.fc4 = nn.Linear(256, n_inp)
 
     def forward(self, samples):
-        x = F.relu(self.fc1(samples))
-        x = F.relu(self.fc2(x))
+        x = F.sigmoid(self.fc1(samples))
+        x = F.sigmoid(self.fc2(x))
         x = F.sigmoid(self.fc3(x))
+        x = F.sigmoid(self.fc4(x))
         return x
