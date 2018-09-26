7f13831b5233c98432cee5501e48ba39ded450f0
abhipanda4 <abhipanda03@gmail.com>
2018-09-26 12:36:09 +0530

    changed model to fewer layers with sigmoid activation

--- autoencoder.py
+++ autoencoder.py
@@ -5,24 +5,20 @@ import torch.nn.functional as F
 class Autoencoder(nn.Module):
     def __init__(self, n_inp, n_encoded):
         super(Autoencoder, self).__init__()
-        self.encoder_fc1 = nn.Linear(n_inp, 512)
-        self.encoder_fc2 = nn.Linear(512, 256)
-        self.encoder_fc3 = nn.Linear(256, n_encoded)
+        self.encoder_fc1 = nn.Linear(n_inp, 1024)
+        self.encoder_fc2 = nn.Linear(1024, n_encoded)
 
-        self.decoder_fc1 = nn.Linear(n_encoded, 256)
-        self.decoder_fc2 = nn.Linear(256, 512)
-        self.decoder_fc3 = nn.Linear(512, n_inp)
+        self.decoder_fc1 = nn.Linear(n_encoded, 1024)
+        self.decoder_fc2 = nn.Linear(1024, n_inp)
 
     def forward(self, x):
-        e = F.relu(self.encoder_fc1(x))
-        e = F.relu(self.encoder_fc2(e))
-        e = F.relu(self.encoder_fc3(e))
+        e = F.sigmoid(self.encoder_fc1(x))
+        e = F.sigmoid(self.encoder_fc2(e))
 
         d = self.decode(e)
         return e, d
 
     def decode(self, x):
-        d = F.relu(self.decoder_fc1(x))
-        d = F.relu(self.decoder_fc2(d))
-        d = F.sigmoid(self.decoder_fc3(d))
+        d = F.sigmoid(self.decoder_fc1(x))
+        d = F.sigmoid(self.decoder_fc2(d))
         return d
