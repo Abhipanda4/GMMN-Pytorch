d912d7c290d3d524ff482ae4cf80de0064a07e19
abhipanda4 <abhipanda03@gmail.com>
2018-09-26 12:36:20 +0530

    added new layers

--- gmmn.py
+++ gmmn.py
@@ -3,16 +3,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class GMMN(nn.Module):
-    def __init__(self, n_start, n_inp):
+    def __init__(self, n_start, n_out):
         super(GMMN, self).__init__()
         self.fc1 = nn.Linear(n_start, 64)
         self.fc2 = nn.Linear(64, 256)
         self.fc3 = nn.Linear(256, 256)
-        self.fc4 = nn.Linear(256, n_inp)
+        self.fc4 = nn.Linear(256, 784)
+        self.fc5 = nn.Linear(784, n_out)
 
     def forward(self, samples):
-        x = F.sigmoid(self.fc1(samples))
-        x = F.sigmoid(self.fc2(x))
-        x = F.sigmoid(self.fc3(x))
-        x = F.sigmoid(self.fc4(x))
+        x = F.relu(self.fc1(samples))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.sigmoid(self.fc5(x))
         return x
