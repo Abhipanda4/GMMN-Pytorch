c481a32baea6966a886e8b78b1b2fac799fbffaa
abhipanda4 <abhipanda03@gmail.com>
2018-09-26 12:37:58 +0530

    gpu suppoort added; visualizatn moved to separate file

--- train.py
+++ train.py
@@ -16,27 +16,19 @@ if not os.path.exists(root):
 if not os.path.exists(model):
     os.mkdir(model)
 
+# use gpu if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # dataloader
 trans = transforms.Compose([transforms.ToTensor()])
 train_set = datasets.MNIST(root=root, train=True, transform=trans, download=True)
-test_set = datasets.MNIST(root=root, train=False, transform=trans, download=True)
-
 train_loader = torch.utils.data.DataLoader(
         dataset=train_set,
         batch_size=BATCH_SIZE,
         shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(
-        dataset=test_set,
-        batch_size=BATCH_SIZE,
-        shuffle=True)
-
-
-# parameters for visualization
-view_data = [test_set[i][0] for i in range(N_ROWS * N_COLS)]
-
 # train the autoencoder first
-encoder_net = Autoencoder(N_INP, ENCODED_SIZE)
+encoder_net = Autoencoder(N_INP, ENCODED_SIZE).to(device)
 encoder_optim = optim.Adam(encoder_net.parameters())
 
 if os.path.exists(ENCODER_SAVE_PATH):
@@ -47,7 +39,8 @@ else:
         avg_loss = 0
         for idx, (x, _) in enumerate(train_loader):
             x = x.view(x.size()[0], -1)
-            _, decoded = encoder_net(Variable(x))
+            x = Variable(x).to(device)
+            _, decoded = encoder_net(x)
             loss = torch.sum((x - decoded) ** 2)
             encoder_optim.zero_grad()
             loss.backward()
@@ -55,28 +48,28 @@ else:
             avg_loss += loss.item()
         avg_loss /= (idx + 1)
 
-        print("Autoencoder Training: Epoch - [%2d] complete, average loss - [%.4f]" %(ep, avg_loss))
+        print("Autoencoder Training: Epoch - [%2d] complete, average loss - [%.4f]" %(ep + 1, avg_loss))
 
     torch.save(encoder_net.state_dict(), ENCODER_SAVE_PATH)
 
 print("Autoencoder has been successfully trained")
 
 # define the GMMN
-gmm_net = GMMN(NOISE_SIZE, ENCODED_SIZE)
+gmm_net = GMMN(NOISE_SIZE, ENCODED_SIZE).to(device)
 if os.path.exists(GMMN_SAVE_PATH):
     gmm_net.load_state_dict(torch.load(GMMN_SAVE_PATH))
     print("Loaded previously saved GMM Network")
 
-gmmn_optimizer = optim.Adam(gmm_net.parameters(), lr=0.01)
+gmmn_optimizer = optim.Adam(gmm_net.parameters(), lr=0.001)
 
 def get_scale_matrix(M, N):
-    s1 = torch.ones((N, 1)) * 1.0 / N
-    s2 = torch.ones((M, 1)) * -1.0 / M
+    s1 = (torch.ones((N, 1)) * 1.0 / N).to(device)
+    s2 = (torch.ones((M, 1)) * -1.0 / M).to(device)
     return torch.cat((s1, s2), 0)
 
 def train_one_step(x, samples, sigma=[1]):
-    x = Variable(x)
-    gen_samples = gmm_net(Variable(samples))
+    samples = Variable(samples).to(device)
+    gen_samples = gmm_net(samples)
     X = torch.cat((gen_samples, x), 0)
     XX = torch.matmul(X, X.t())
     X2 = torch.sum(X * X, 1, keepdim=True)
@@ -89,7 +82,7 @@ def train_one_step(x, samples, sigma=[1]):
 
     loss = 0
     for v in sigma:
-        kernel_val = torch.exp(1.0 / v * exp)
+        kernel_val = torch.exp(exp / v)
         loss += torch.sum(S * kernel_val)
 
     loss = torch.sqrt(loss)
@@ -106,7 +99,8 @@ for ep in range(N_GEN_EPOCHS):
     for idx, (x, _) in enumerate(train_loader):
         x = x.view(x.size()[0], -1)
         with torch.no_grad():
-            encoded_x, _ = encoder_net(Variable(x))
+            x = Variable(x).to(device)
+            encoded_x, _ = encoder_net(x)
 
         # uniform random noise between [-1, 1]
         random_noise = torch.rand((BATCH_SIZE, NOISE_SIZE)) * 2 - 1
@@ -115,22 +109,5 @@ for ep in range(N_GEN_EPOCHS):
 
     avg_loss /= (idx + 1)
     print("GMMN Training: Epoch - [%3d] complete, average loss - [%.4f]" %(ep, avg_loss))
-
-    plt.ion()
-    plt.gray()
-
-    for r in range(N_ROWS):
-        for c in range(N_COLS):
-            ax = plt.subplot(N_ROWS, N_COLS, r * N_COLS + c + 1)
-
-            noise = torch.rand((1, NOISE_SIZE)) * 2 - 1
-            encoded_x = gmm_net(Variable(noise))
-            y = encoder_net.decode(encoded_x)
-
-            plt.imshow(y.detach().squeeze().numpy().reshape(28, 28))
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
-    plt.show()
-    plt.pause(0.5)
 
 torch.save(gmm_net.state_dict(), GMMN_SAVE_PATH)
